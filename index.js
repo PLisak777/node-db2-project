fa8febbaa0388a9f5923ee78b9853561ed6d20ff
Phil <philip-lisak@lambdastudents.com>
2020-11-12 23:03:19 -0700

    .put endpoint completed

--- index.js
+++ index.js
@@ -37,3 +37,32 @@ server.post('/api/cars', async (req, res) => {
 		});
 	}
 });
+
+server.put('/api/cars/:id', async (req, res) => {
+	try {
+		const { vin, make, model, mileage } = req.body;
+		const { id } = req.params;
+
+		if (vin && make && model && mileage) {
+			const count = await db('cars').update(req.body).where({ id });
+			if (count > 0) {
+				res.status(200).json(await db('cars').where({ id }));
+			} else {
+				res.status(404).json({
+					message: 'That id does not exist',
+				});
+			}
+		} else {
+			res.status(400).json({
+				message: `Please provide the following: {${!vin && 'vin,'} ${
+					!make && 'make,'
+				} ${!model && 'model,'} ${!mileage && 'mileage'}}`,
+			});
+		}
+	} catch (err) {
+		res.status(500).json({
+			message: 'Something went wrong!',
+			error: err,
+		});
+	}
+});
