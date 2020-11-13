d7c8590af471e87a94c23d458ed73cb910906097
Phil <philip-lisak@lambdastudents.com>
2020-11-12 21:47:28 -0700

    Initial commit with dependencies and first migration added

---
+++ data/migrations/20201112213706_cars-schema.js
@@ -0,0 +1,13 @@
+exports.up = function (knex) {
+    return knex.schema.createTable('cars', tbl => {
+        tbl.increments();
+        tbl.varchar('vin', 17).unique().notNullable();
+        tbl.string('make').notNullable();
+        tbl.string('model').notNullable();
+        tbl.integer('mileage').notNullable();
+    })
+};
+
+exports.down = function (knex) {
+    return knex.schema.dropTableIfExists('cars');
+};
